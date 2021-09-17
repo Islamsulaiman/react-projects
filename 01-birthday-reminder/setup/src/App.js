93a6195d4fbcd431ac235319d96892f852b9f4a5
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-17 10:17:47 +0300

    First project, birthday cards

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -9,7 +9,24 @@ function App() {
     <>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
-        {people.map((person) => {})}
+        {people.map((person) => {
+          let { id, name, age, image } = person;
+          return (
+            <article key={id} className='person'>
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>
+                  <h5>{age} years</h5>
+                </p>
+              </div>
+            </article>
+          );
+        })}
+        <button type='button' onClick={() => setPeople([])}>
+          Clear All
+        </button>
+        ;
       </section>
     </>
   );
