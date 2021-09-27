96532841b0b503d6bdee884b990f19cb8123e1e5
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-27 20:47:33 +0300

    4th project 'accordion'

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <>
+      <main className='container'>
+        <section>
+          <h2>Questions And Answers About Login</h2>
+        </section>
+        <section>
+          {data.map((q) => {
+            return <SingleQuestion key={q.id} {...q} />;
+          })}
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
