84246e6c5e8661c3085a7b03c15e21028300418c
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-27 10:03:07 +0300

    Second project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  let [loading, setLoading] = useState(true);
+  let [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => {
+      return tour.id !== id;
+    });
+    setTours(newTours);
+  };
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <>
+      <Tours tours={tours} removeTour={removeTour} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+  let [allInfo, setAllInfo] = useState(false);
+  // let { id, name, info, image, price } = tour;
+
+  return (
+    <>
+      <img src={image} alt={name} />
+      <footer>
+        <p>{name}</p>
+        <p>{price}</p>
+        <p>
+          {allInfo ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setAllInfo(!allInfo);
+            }}
+          >
+            {allInfo ? "Read less" : "Read more"}
+          </button>
+        </p>
+
+        <button onClick={() => removeTour(id)}>Not interested</button>
+      </footer>
+    </>
+  );
 };
 
 export default Tour;
