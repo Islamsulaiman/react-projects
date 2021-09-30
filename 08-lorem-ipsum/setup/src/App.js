3710edceced1695a95ad3704952844cc8780aa64
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-30 22:25:21 +0300

    Lorem Ipsum Project

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  let [num, setNum] = useState(0);
+  let [para, setPara] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newData = [];
+    for (let i = 1; i <= num; i++) {
+      newData.push(data[i]);
+    }
+    setPara(newData);
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <>
+      <main>
+        <h3>Tired of Lorem Ipsum?</h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='counter'>Paragraphs: </label>
+          <input
+            type='number'
+            name='counter'
+            value={num}
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
+          <button type='submit' className='btn'>
+            Generate
+          </button>
+        </form>
+      </main>
+      <section>
+        {para.map((i) => {
+          return (
+            <React.Fragment>
+              <p>{i}</p>
+            </React.Fragment>
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
