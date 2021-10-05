c7b9ab895455d3458637da15aa75b004708331d9
islam Sulaiman <ma.eslam@yahoo.com>
2021-10-05 15:49:16 +0300

    Context part1

--- 02-tours-my-work/src/context.js
+++ 02-tours-my-work/src/context.js
@@ -1,9 +1,32 @@
 import React, { useEffect, useReducer, useContext, useState } from "react";
 import { createContext } from "react";
 
+// context object instead of prop drilling
 const context = createContext();
 
+// fetch data from url
+const url = "https://course-api.com/react-tours-project";
+
+//object that control all of my state using useReducer
+const controller = {
+  isLoading: true,
+};
+
+//function to control useReducer states
+const reducer = (state, action) => {};
+
 const AppContext = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(reducer, controller);
+
+  let dataFetched = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setData(data);
+    dispatch({ type: "END_LOADING" });
+  };
+
   return (
     <>
       <context.Provider value='hello'>
