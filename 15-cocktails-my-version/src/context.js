999540eed4b574971f8f2e9c7559844b4703e1fc
islam Sulaiman <ma.eslam@yahoo.com>
2021-10-12 21:19:53 +0300

    fetch the data from api

--- 15-cocktails-my-version/src/context.js
+++ 15-cocktails-my-version/src/context.js
@@ -1,15 +1,41 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback, useMemo } from "react";
+import { useFetch } from "./components/useFetch";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  let [cocktails, setCocktails] = useState([]);
+  let [searchTerm, setSearchTerm] = useState("a");
+  let [loading, setLoading] = useState(true);
+
+  let fetchData = useCallback(async () => {
+    try {
+      let res = await fetch(`${url}${searchTerm}`);
+      let resData = await res.json();
+      setLoading(false);
+      setCocktails(resData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [url, searchTerm]);
+
+  useEffect(() => {
+    fetchData();
+  }, [searchTerm, fetchData]);
+  console.log(cocktails);
+  return (
+    <AppContext.Provider
+      value={{ loading, cocktails, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
