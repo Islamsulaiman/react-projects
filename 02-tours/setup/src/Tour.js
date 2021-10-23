a21dedbf04a1d417750f2751dcb88e8453912883
islam Sulaiman <ma.eslam@yahoo.com>
2021-10-23 19:11:55 +0300

    adding bootstrap

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -3,26 +3,57 @@ import React, { useState } from "react";
 const Tour = ({ id, name, info, image, price, removeTour }) => {
   let [allInfo, setAllInfo] = useState(false);
   // let { id, name, info, image, price } = tour;
+  let style = "width: 15px; height: 10px;";
 
   return (
     <>
-      <img src={image} alt={name} />
-      <footer>
-        <p>{name}</p>
-        <p>{price}</p>
-        <p>
-          {allInfo ? info : `${info.substring(0, 200)}...`}
-          <button
-            onClick={() => {
-              setAllInfo(!allInfo);
+      <div className='card mb-4 rounded shadow-lg border border-white'>
+        <div>
+          <img
+            src={image}
+            alt={name}
+            className='rounded img-fluid '
+            style={{
+              width: "50rem",
+              height: "20rem",
+              overflow: "hidden",
+              objectFit: "cover",
+              maxHeight: "100%",
+              maxWidth: "100%",
+              objectPosition: "50% 60%",
             }}
-          >
-            {allInfo ? "Read less" : "Read more"}
-          </button>
-        </p>
+          />
+        </div>
 
-        <button onClick={() => removeTour(id)}>Not interested</button>
-      </footer>
+        <footer>
+          <div className=' d-flex justify-content-between m-3 mb-0'>
+            <p className='fw-bold'>{name}</p>
+            <p className='bg-info bg-gradient text-primary p-3 fw-bold'>
+              {price}
+            </p>
+          </div>
+
+          <p className='m-3 mb-0 mt-1'>
+            {allInfo ? info : `${info.substring(0, 200)}...`}
+            <button
+              className='btn btn-link text-info m-1 ps-0 mt-0'
+              onClick={() => {
+                setAllInfo(!allInfo);
+              }}
+            >
+              {allInfo ? "Read less" : "Read more"}
+            </button>
+          </p>
+          <div className='d-flex justify-content-center m-4'>
+            <button
+              className='btn btn-outline-danger text-center m-0'
+              onClick={() => removeTour(id)}
+            >
+              Not interested
+            </button>
+          </div>
+        </footer>
+      </div>
     </>
   );
 };
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -2,17 +2,19 @@ import React from "react";
 import Tour from "./Tour";
 const Tours = ({ tours, removeTour }) => {
   return (
-    <>
-      <div className='title'>
-        <h2>our tours</h2>
-        <div className='underline'></div>
+    <React.Fragment>
+      <div className='container-fluid col-5 mt-5'>
+        <div className='title '>
+          <h2 className='fw-bold'>our tours</h2>
+          <div className='underline'></div>
+        </div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+          })}
+        </div>
       </div>
-      <div>
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
-        })}
-      </div>
-    </>
+    </React.Fragment>
   );
 };
 
